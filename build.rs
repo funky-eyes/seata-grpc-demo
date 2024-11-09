8f882a20db1bd5b73b174d68aa05ebf396e7173a
funky-eyes <jianbin@apache.org>
2024-11-10 01:31:09 +0800

    connect seata-server

--- build.rs
+++ build.rs
@@ -1,9 +1,4 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::configure()
-        .build_server(false)
-        .compile_protos(
-            &["proto/seata/grpcMessage.proto"],
-            &["proto/seata"],
-        )?;
+    tonic_build::compile_protos("proto/seata/grpcMessage.proto").unwrap();
     Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,68 @@
-pub mod pb {
+pub mod grpc_message {
     tonic::include_proto!("org.apache.seata.protocol.protobuf.grcp_message");
 }
+
+pub mod seata_protobuf {
+    tonic::include_proto!("org.apache.seata.protocol.protobuf");
+}
+
 use std::collections::HashMap;
-use prost::alloc::vec::Vec;
-use std::time::Duration;
-use org::apache::seata::protocol::protobuf::{RegisterTmRequestProto, AbstractIdentifyRequestProto, RegisterTmResponseProto};
+use seata_protobuf::{MessageTypeProto, RegisterTmRequestProto, AbstractMessageProto, AbstractIdentifyRequestProto, RegisterTmResponseProto};
 use tokio_stream::{Stream, StreamExt};
 use tonic::transport::Channel;
-use pb::{seata_service_client::SeataServiceClient, GrpcMessageProto};
+use grpc_message::{seata_service_client::SeataServiceClient, GrpcMessageProto};
 use prost::alloc::string::String;
 use prost::alloc::boxed::Box;
+use prost::Message;
 
-async fn streaming_msg(client: &mut SeataServiceClient<Channel>, num: usize) {
-    let register_tm_request_proto = RegisterTmRequestProto {
-        abstract_identify_request: Some(AbstractIdentifyRequestProto {
-            abstract_message: Some(AbstractMessageProto {
-                message_type: MessageTypeProto::TypeGlobalBegin as i32,
-            }),
-            version: String::from("1.0"),
-            application_id: String::from("app_id"),
-            transaction_service_group: String::from("tx_group"),
-            extra_data: String::from("extra_data"),
-        }),
+fn seata_requests_iter() -> impl Stream<Item = GrpcMessageProto> {
+    let abstract_identify_request_proto = AbstractIdentifyRequestProto {
+        abstract_message: None,
+        version: String::new(),
+        application_id: String::from("test-applicationId"),
+        transaction_service_group: String::new(),
+        extra_data: String::new(),
     };
+
     let mut head_map = HashMap::new();
+    let result = 2f64.powi(1);
+    let register_tm_request_proto = RegisterTmRequestProto {
+        abstract_identify_request: Some(abstract_identify_request_proto),
+    };
+    head_map.insert("codec-type".to_string(), result.to_string());
+    let bytes = register_tm_request_proto.encode_to_vec();
     let grpc_message_proto = GrpcMessageProto {
         id: 1,
         message_type: 2,
         head_map: head_map,
-        body: Vec::from("example body"),
+        body: bytes,
     };
-    let stream = client
-        .send_request(grpc_message_proto)
-        .await
-        .unwrap()
-        .into_inner();
+    let test =  RegisterTmRequestProto::decode(grpc_message_proto.body.as_slice()).unwrap();
+    println!("\treceived message: `{}`", test.abstract_identify_request.unwrap().application_id);
+    tokio_stream::iter(1..usize::MAX).map(move |i| grpc_message_proto.clone())
+}
 
-    // stream is infinite - take just 5 elements and then disconnect
-    let mut stream = stream.take(num);
-    while let Some(item) = stream.next().await {
-        println!("\treceived: {}", item.unwrap().message);
+async fn seata_streaming_echo(client: &mut SeataServiceClient<Channel>, num: usize) {
+    let in_stream = seata_requests_iter().take(num);
+
+    let response = client
+        .send_request(in_stream)
+        .await
+        .unwrap();
+
+    let mut resp_stream = response.into_inner();
+
+    while let Some(received) = resp_stream.next().await {
+        let received = received.unwrap();
+        println!("\treceived message: `{}`", received.message_type);
     }
-    // stream is dropped here and the disconnect info is sent to server
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut client = SeataServiceClient::connect("http://127.0.0.1:8091").await?;
-
+    println!("\r\nseata stream echo:");
+    seata_streaming_echo(&mut client, 1).await;
 
     Ok(())
 }
